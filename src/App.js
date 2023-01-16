18c1ec8f4809f57ebd9f195f46cd2527a12e1565
zainasif767 <zainclasher2002@gmail.com>
2023-01-17 01:40:59 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState, useEffect } from 'react';
+import MovieCard from './components/MovieCard';
+import SearchIcon from './search.svg'
+import { nanoid } from 'nanoid'
 
-function App() {
+export default function App() {
+  const [movies, setMovies] = useState([])
+  const apiURL = "http://www.omdbapi.com?apikey=2b1c8c2f"
+
+  const movie = {
+    "Poster": "https://m.media-amazon.com/images/M/MV5BN2I4OTllM2MtMWVhNC00MjkzLWJlMDUtN2FhMGQ2ZGVjMjllXkEyXkFqcGdeQXVyMTEyNzgwMDUw._V1_SX300.jpg",
+    "Title": "Batman v Superman: Dawn of Justice (Ultimate Edition)",
+    " Type": "movie",
+    "Year": "2016",
+    " imdbID": "tt18689424"
+  }
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiURL}&s=${title}`)
+    const data = await response.json()
+    setMovies(data.Search)
+  }
+
+  useEffect(() => {
+    searchMovies('Batman')
+  }, [])
+
+  const movieElements = movies.map(movie => <MovieCard key={nanoid()} movie={movie} />)
+  const showMovie = movies?.length > 0
+    ? (
+      <div className='container'>
+        {movieElements}
+      </div>
+    ) : (
+      <div className='empty'>
+        <h2>No Movies Found ! </h2>
+      </div>
+    )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='app'>
+      <h1>ZAINFLIX</h1>
+      <div className='search'>
+        <input
+          placeholder='Search Movie'
+          value='Batman'
+          onChange={() => { }}
+        />
+        <img
+          src={SearchIcon}
+          alt="Search"
+          onClick={() => { }}
+        />
+      </div>
+      {showMovie}
 
-export default App;
+
+    </div>
+  )
+}
---
+++ src/components/MovieCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function MovieCard(props) {
+    return (
+        <div className="movie">
+            <div>
+                <p>{props.movie.Year}</p>
+            </div>
+            <div>
+                <img src={props.movie.Poster !== 'N/A' ? props.movie.Poster : 'https://via.placeholder.com/400'} alt={props.movie.Title} />
+            </div>
+            <div>
+                <span>{props.movie.Type}</span>
+                <h3>{props.movie.Title}</h3>
+            </div>
+        </div>
+    )
+}
