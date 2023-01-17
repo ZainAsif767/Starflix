0ed266efc27f7f252d5629327aaaa6438890cf9e
zainasif767 <zainclasher2002@gmail.com>
2023-01-17 22:21:38 +0500

    Fix

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import MovieCard from './components/MovieCard';
 import SearchIcon from './search.svg'
-import { nanoid } from 'nanoid'
 
 export default function App() {
   const [movies, setMovies] = useState([])
-  const apiURL = "http://www.omdbapi.com?apikey=2b1c8c2f"
+  const [searchTerm, setSearchTerm] = useState('')
 
-  const movie = {
-    "Poster": "https://m.media-amazon.com/images/M/MV5BN2I4OTllM2MtMWVhNC00MjkzLWJlMDUtN2FhMGQ2ZGVjMjllXkEyXkFqcGdeQXVyMTEyNzgwMDUw._V1_SX300.jpg",
-    "Title": "Batman v Superman: Dawn of Justice (Ultimate Edition)",
-    " Type": "movie",
-    "Year": "2016",
-    " imdbID": "tt18689424"
-  }
+  const apiURL = "http://www.omdbapi.com?apikey=2b1c8c2f" // Api called and saved
 
+  //  Just to understand what props to pass from API 👇
+  // const movie = {
+  //   "Poster": "https://m.media-amazon.com/images/M/MV5BN2I4OTllM2MtMWVhNC00MjkzLWJlMDUtN2FhMGQ2ZGVjMjllXkEyXkFqcGdeQXVyMTEyNzgwMDUw._V1_SX300.jpg",
+  //   "Title": "Batman v Superman: Dawn of Justice (Ultimate Edition)",
+  //   "Type": "movie",
+  //   "Year": "2016",
+  //   "imdbID": "tt18689424" 
+  // }
+
+  // Fetching movies from API and setting data
   const searchMovies = async (title) => {
     const response = await fetch(`${apiURL}&s=${title}`)
     const data = await response.json()
@@ -26,8 +29,11 @@ export default function App() {
   useEffect(() => {
     searchMovies('Batman')
   }, [])
+  // mapping the movies array👇
+  const movieElements = movies.map(movie => (
+    <MovieCard key={movie.imdbID} movie={movie} />))
 
-  const movieElements = movies.map(movie => <MovieCard key={nanoid()} movie={movie} />)
+  // what to display if movie searched is found or not
   const showMovie = movies?.length > 0
     ? (
       <div className='container'>
@@ -38,20 +44,33 @@ export default function App() {
         <h2>No Movies Found ! </h2>
       </div>
     )
-
+  function handleChange(event) {
+    setSearchTerm(event.target.value)
+  }
+  function handleClick() {
+    searchMovies(searchTerm)
+  }
+  function handleSubmit(event) {
+    if (event.key === 'Enter') {
+      handleClick()
+    }
+  }
   return (
     <div className='app'>
-      <h1>ZAINFLIX</h1>
+      <div>
+        <h1>STARFLIX</h1>
+      </div>
       <div className='search'>
         <input
+          onKeyDown={handleSubmit}
           placeholder='Search Movie'
-          value='Batman'
-          onChange={() => { }}
+          value={searchTerm}
+          onChange={handleChange}
         />
         <img
           src={SearchIcon}
           alt="Search"
-          onClick={() => { }}
+          onClick={handleClick}
         />
       </div>
       {showMovie}
